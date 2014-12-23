fb2e96bfb3a61a89b0f407317e65f827afc1fa15
Adam Louis <adamlouis@gmail.com>
2014-12-22 17:01:27 -0800

    refactor checklists spec

--- spec/requests/api/v1/checklists_spec.rb
+++ spec/requests/api/v1/checklists_spec.rb
@@ -3,24 +3,22 @@ require 'rails_helper'
 describe 'Checklists API' do
   describe '#create' do
     context 'for an existing user' do
-      let!(:user){ User.create(email: 'a@a.com', password: '12345678') }
+      let!(:user){ User.create!(email: 'a@a.com', password: '12345678') }
 
-      it "creates the checklist belonging to that user" do
+      it "creates a checklist belonging to that user" do
         post "/api/v1/users/#{user.id}/checklists", {checklist: {name: 'a checklist'}}
-        expect(Checklist.count).to eq(1)
         expect(user.checklists.count).to eq(1)
-        expect(user.checklists.first.name).to eq('a checklist')
       end
     end
-  end 
+  end
 
   describe '#update' do
     context "for an existing user's existing checklist" do
-      let!(:user){ User.create(email: 'a@a.com', password: '12345678') }
-      let!(:checklist){ user.checklists.create(name: 'checklist 1') }
+      let!(:user){ User.create!(email: 'a@a.com', password: '12345678') }
+      let!(:checklist){ user.checklists.create!(name: 'checklist 1') }
 
       it 'updates the checklist' do
-        put "/api/v1/users/#{user.id}/checklists/#{checklist.id}", {checklist: {name: 'checklist 2'}}
+        patch "/api/v1/users/#{user.id}/checklists/#{checklist.id}", {checklist: {name: 'checklist 2'}}
         expect(user.checklists.first.name).to eq('checklist 2')
       end
     end
@@ -28,8 +26,8 @@ describe 'Checklists API' do
 
   describe '#destroy' do
     context "for an existing user's existing checklist" do
-      let!(:user){ User.create(email: 'a@a.com', password: '12345678') }
-      let!(:checklist){ user.checklists.create(name: 'checklist 1') }
+      let!(:user){ User.create!(email: 'a@a.com', password: '12345678') }
+      let!(:checklist){ user.checklists.create!(name: 'checklist 1') }
 
       it 'deletes the checklist' do
         delete "/api/v1/users/#{user.id}/checklists/#{checklist.id}"
@@ -40,15 +38,12 @@ describe 'Checklists API' do
 
   describe '#index' do
     context 'for an existing user with some checklists' do
-      let!(:user){ User.create(email: 'a@a.com', password: '12345678') }
-      let!(:checklist1){  user.checklists.create(name: 'Checklist 1')}
-      let!(:checklist2){  user.checklists.create(name: 'Checklist 2')}
+      let!(:user){ User.create!(email: 'a@a.com', password: '12345678') }
+      before { 5.times { |n| user.checklists.create!(name: "checklist #{n}") } }
 
-      it "returns the checklists' names" do
+      it "returns the user's checklists" do
         get "/api/v1/users/#{user.id}/checklists"
-        # ['checklists': {name: 'Checklist 1'}, {name: 'Checklist'}]
-        expect(response.body).to include(checklist1.name)
-        expect(response.body).to include(checklist2.name)
+        expect(response.body).to eq(user.checklists.to_json)
       end
     end
   end
