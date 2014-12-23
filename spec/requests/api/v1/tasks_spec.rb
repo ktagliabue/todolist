bcc086c924aed9a42d0d121dbabb35ba8f52f4fa
Adam Louis <adamlouis@gmail.com>
2014-12-22 17:03:17 -0800

    refactor tasks spec

--- spec/requests/api/v1/tasks_spec.rb
+++ spec/requests/api/v1/tasks_spec.rb
@@ -3,39 +3,38 @@ require 'rails_helper'
 describe 'Tasks API' do
   describe '#create' do
     context "for an existing user's existing checklist" do
-      let!(:user){ User.create(email: 'a@a.com', password: '12345678') }
-      let!(:checklist){ user.checklists.create(name: 'a checklist') }
-      let!(:tasks){ user.tasks.create(name: 'a task') }
+      let!(:user){ User.create!(email: 'a@a.com', password: '12345678') }
+      let!(:checklist){ user.checklists.create!(name: 'a checklist') }
 
-      it "creates the task for that checklist" do
-        post "/api/v1/users/#{user.id}/checklists/#{checklist.id}/tasks/", {task: {name: 'a task'}}
-        expect(user.tasks.first.name).to eq('a task')
+      it "creates a task belonging to that checklist" do
+        post "/api/v1/users/#{user.id}/checklists/#{checklist.id}/tasks", {task: {name: 'a task'}}
+        expect(user.tasks.count).to eq(1)
       end
     end
-  end
+  end 
 
   describe '#update' do
-    context "for an existing user's existing task" do
-      let!(:user){ User.create(email: 'a@a.com', password: '12345678') }
-      let!(:checklist){ user.checklists.create(name: 'checklist 1') }
-      let!(:tasks){ user.tasks.create(name: 'a task') }
+    context "for an existing user's existing checklist's existing task" do
+      let!(:user){ User.create!(email: 'a@a.com', password: '12345678') }
+      let!(:checklist){ user.checklists.create!(name: 'a checklist') }
+      let!(:task){ checklist.tasks.create!(name: 'task 1') }
 
-      it 'updates the task' do
-        put "/api/v1/users/#{user.id}/checklists/#{checklist.id}/tasks/#{tasks.id}", {task: {name: 'another task'}}
-        expect(user.tasks.first.name).to eq('another task')
+      it 'updates the checklist' do
+        patch "/api/v1/users/#{user.id}/checklists/#{checklist.id}/tasks/#{task.id}", {task: {name: 'task 2'}}
+        expect(checklist.tasks.first.name).to eq('task 2')
       end
     end
   end
 
   describe '#destroy' do
-    context "for an existing user's existing task" do
-      let!(:user){ User.create(email: 'a@a.com', password: '12345678') }
-      let!(:checklist){ user.checklists.create(name: 'checklist 1') }
-      let!(:tasks){ user.tasks.create(name: 'a task') }
+    context "for an existing user's existing checklist's existing task" do
+      let!(:user){ User.create!(email: 'a@a.com', password: '12345678') }
+      let!(:checklist){ user.checklists.create!(name: 'a checklist') }
+      let!(:task){ checklist.tasks.create!(name: 'task 1') }
 
-      it 'deletes the task' do
-        delete "/api/v1/users/#{user.id}/checklists/#{checklist.id}/tasks/#{tasks.id}"
-        expect(user.tasks.count).to eq(0)
+      it 'deletes the checklist' do
+        delete "/api/v1/users/#{user.id}/checklists/#{checklist.id}/tasks/#{task.id}"
+        expect(checklist.tasks.count).to eq(0)
       end
     end
   end
